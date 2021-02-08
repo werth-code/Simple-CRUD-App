e15980e6e0ecdccb408f31e95ff81118fffa6410
Matt Werth <matthewwerth@gmail.com>
2021-02-08 10:40:15 -0500

    Added Mongo

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
-const express =require('express')
+const express = require('express')
+const bodyParser= require('body-parser')
 const app = express()
 
-app.get("/", (req, res) => { 
-    res.send("HIYA !!!! There!")
+app.use(bodyParser.urlencoded({ extended: true }))
+const MongoClient = require('mongodb').MongoClient
+
+let connectionString = 'mongodb+srv://matt:rhinos91@cluster0.hbmkm.mongodb.net/crud-app?retryWrites=true&w=majority'
+
+MongoClient.connect(connectionString, {useUnifiedTopology: true}, (err, client) => {
+    if (err) return console.error(err)
+    console.log("Something happened MONGO...")
 })
 
-app.listen(3100,function(){  console.log('listening on 3000')})
+app.get("/", (req, res) => { 
+    res.sendFile('/Users/m21/dev/VS Code/Express/SimpleCRUDApp/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+console.log(req.body)
+})
+
+app.listen(3100,function(){  console.log('listening on 3100')})
