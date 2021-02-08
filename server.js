59e0c17f14814d51ac4aad5e9300a9b052818117
Matt Werth <matthewwerth@gmail.com>
2021-02-08 11:30:49 -0500

    Added Database Connection

--- server.js
+++ server.js
@@ -7,17 +7,36 @@ const MongoClient = require('mongodb').MongoClient
 
 let connectionString = 'mongodb+srv://matt:rhinos91@cluster0.hbmkm.mongodb.net/crud-app?retryWrites=true&w=majority'
 
-MongoClient.connect(connectionString, {useUnifiedTopology: true}, (err, client) => {
-    if (err) return console.error(err)
-    console.log("Something happened MONGO...")
-})
+MongoClient.connect(connectionString, {useUnifiedTopology: true })
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('crud-app')
+        const quotesCollection = db.collection('quotes')
 
-app.get("/", (req, res) => { 
-    res.sendFile('/Users/m21/dev/VS Code/Express/SimpleCRUDApp/index.html')
-})
+    app.get("/", (req, res) => { 
+        const cursor = db.collection('quotes')
+                         .find()
+                         .toArray()
+                         .then(results => {
 
-app.post('/quotes', (req, res) => {
-console.log(req.body)
-})
+                            console.log(results) // These are our database items...
 
-app.listen(3100,function(){  console.log('listening on 3100')})
+                         }).catch(error => console.error(error))
+/
+        res.sendFile('/Users/m21/dev/VS Code/Express/SimpleCRUDApp/index.html')
+    })
+
+    app.post('/quotes', (req, res) => {
+        quotesCollection.insertOne(req.body)
+        .then(result => {
+            console.log(req.body)
+            res.redirect('/')
+    }).catch(error => console.error(error))})
+
+}).catch(console.error)
+
+
+
+app.listen(3100, function(){  
+    console.log('listening on 3100')
+})
